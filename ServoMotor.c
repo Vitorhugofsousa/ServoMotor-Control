afa1e33ca9083f7958b483fd5ae4caeb030624cb
Vitorhugofsousa <vitorhugofs2018@gmail.com>
2025-02-10 17:50:09 -0300

    configuracao inicial do projeto

---
+++ ServoMotor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h" 
+#include "hardware/pwm.h"
+
+//definição dos pinos e valores
+#define SERVO_PIN 22
+#define CLOCK_DIV 64.0 
+#define TPWM 20000.0    
+#define WRAP_VALUE 39063
+
+int main(){
+    stdio_init_all();
+
+    while (true) {
+       
+    }
+}
